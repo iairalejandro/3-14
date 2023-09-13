b9b3773d609884967c256ab98d5fc0e1d8d578fc
iairalejandro <101744230+iairalejandro@users.noreply.github.com>
2023-09-13 15:06:29 -0500

    Create 3-14.cpp

---
+++ 3-14.cpp
@@ -0,0 +1,13 @@
+#include <iostream>	
+#include "3-14.h"
+#include <string>
+
+using namespace std;
+
+int main() {
+
+	list MiLista(123456, "Carl", "Smith", 20);
+	MiLista.display();
+    
+	return 0;
+}
